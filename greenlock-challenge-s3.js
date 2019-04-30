232905b21a6dd7fbf04a841b6c3d4b36c9735955
Cyrille <c.derche@me.com>
2019-05-01 00:51:35 +0100

    added directory

--- greenlock-challenge-s3.js
+++ greenlock-challenge-s3.js
@@ -22,7 +22,7 @@ module.exports.create = (createOptions) => {
             AWS.config.update({ region: opts.bucketRegion, credentials: new AWS.Credentials({ accessKeyId: opts.accessKeyId, secretAccessKey: opts.secretAccessKey }) });
             const s3 = new AWS.S3({ apiVersion: '2006-03-01', params: { Bucket: opts.bucketName } });
 
-            challengeKey = encodeURIComponent(directory + key);
+            challengeKey = encodeURIComponent(opts.directory + key);
             s3.putObject({ Key: challengeKey, Body: value, Bucket: opts.bucketName }, function (err, data) {
                 if (err) {
                     console.error('There was an error creating your challenge: ' + err.message);
@@ -38,7 +38,7 @@ module.exports.create = (createOptions) => {
             AWS.config.update({ region: opts.bucketRegion, credentials: new AWS.Credentials({ accessKeyId: opts.accessKeyId, secretAccessKey: opts.secretAccessKey }) });
             const s3 = new AWS.S3({ apiVersion: '2006-03-01', params: { Bucket: opts.bucketName } });
 
-            challengeKey = encodeURIComponent(directory + key);
+            challengeKey = encodeURIComponent(opts.directory + key);
             s3.getObject({ Key: challengeKey, Bucket: opts.bucketName }, function (err, data) {
                 if (err) {
                     console.error('There was an error retrieving your challenge: ' + err.message);
@@ -54,7 +54,7 @@ module.exports.create = (createOptions) => {
             AWS.config.update({ region: opts.bucketRegion, credentials: new AWS.Credentials({ accessKeyId: opts.accessKeyId, secretAccessKey: opts.secretAccessKey }) });
             const s3 = new AWS.S3({ apiVersion: '2006-03-01', params: { Bucket: opts.bucketName } });
 
-            challengeKey = encodeURIComponent(directory + key);
+            challengeKey = encodeURIComponent(opts.directory + key);
             s3.deleteObject({ Key: challengeKey, Bucket: opts.bucketName }, function (err, data) {
                 if (err) {
                     console.error('There was an error deleting your challenge: ', err.message);
