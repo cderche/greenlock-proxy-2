88d2aaafcfc1ae413e493e12c49f650726f989c8
Cyrille <c.derche@me.com>
2019-05-02 11:55:41 +0100

    modernized challenges

--- index.js
+++ index.js
@@ -28,14 +28,16 @@ var greenlock = Greenlock.create({
   })
   , debug: false
   , challengeType: 'http-01'
-  , challenge: require('./greenlock-challenge-s3').create({
-    debug: false
-    , accessKeyId: process.env.AWS_ACCESS_KEY_ID
-    , secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
-    , bucketName: process.env.AWS_BUCKET_NAME
-    , bucketRegion: process.env.AWS_BUCKET_REGION
-    , directory: 'acme-challenge/'
-  })
+  , challenges: {
+    'http-01': require('./greenlock-challenge-s3').create({
+      debug: false
+      , accessKeyId: process.env.AWS_ACCESS_KEY_ID
+      , secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+      , bucketName: process.env.AWS_BUCKET_NAME
+      , bucketRegion: process.env.AWS_BUCKET_REGION
+      , directory: 'acme-challenge/'
+    })
+  }
 });
 
 ////////////////////////
